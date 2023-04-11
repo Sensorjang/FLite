ec63bf07d527a8ca71d7ea0b49d6c950e03e5155
Sensorjang Dell Desktop <515310624@qq.com>
2023-04-11 11:36:22 +0800

    实现BaseServer，定义server服务，定义联邦server策略

---
+++ FLite/server/__init__.py
@@ -0,0 +1,7 @@
+# 暴露到外部的函数和类
+from FLite.server.BaseServer import BaseServer
+from FLite.server.service import ServerService
+from FLite.server.strategies import federated_averaging, federated_averaging_only_params, weighted_sum, weighted_sum_only_params
+
+__all__ = ['BaseServer', 'ServerService', 'federated_averaging', 'federated_averaging_only_params',
+           'weighted_sum', 'weighted_sum_only_params']
