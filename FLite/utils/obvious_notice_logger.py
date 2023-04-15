7e5cdf8f1e1302bd3e85f2a39cf4b9e054845843
Sensorjang Dell Desktop <515310624@qq.com>
2023-04-15 13:13:42 +0800

    添加高亮输出测试

--- FLite/utils/obvious_notice_logger.py
+++ FLite/utils/obvious_notice_logger.py
@@ -56,5 +56,23 @@ class noticelogger(object):
 
 
 if __name__ == "__main__":
-    logger = noticelogger()
-    logger.FLiteTitle()
+    notice_logger = noticelogger.get_instance()
+    notice_logger.FLiteTitle()
+    notice_logger.red("red")
+    notice_logger.green("green")
+    notice_logger.yellow("yellow")
+    notice_logger.blue("blue")
+    notice_logger.purple("purple")
+    notice_logger.cyan("cyan")
+    notice_logger.white("white")
+    notice_logger.black("black")
+
+    print("\033[1;31mDuring model publishing: Decryption (Blowfish) OK! \033[0m") # red
+    print("\033[1;32mDuring model publishing: Decryption (Blowfish) OK! \033[0m") # green
+    print("\033[1;33mDuring model publishing: Decryption (Blowfish) OK! \033[0m") # yellow
+    print("\033[1;34mDuring model publishing: Decryption (Blowfish) OK! \033[0m") # blue
+    print("\033[1;35mDuring model publishing: Decryption (Blowfish) OK! \033[0m") # purple
+    print("\033[1;36mDuring model publishing: Decryption (Blowfish) OK! \033[0m") # cyan
+    print("\033[1;37mDuring model publishing: Decryption (Blowfish) OK! \033[0m") # white
+    print("\033[1;30mDuring model publishing: Decryption (Blowfish) OK! \033[0m") # black
+
